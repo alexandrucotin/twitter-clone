5c8767057353e786ec61489a200046c0ad99035d
Alexandru Cotin <a.cotin96@gmail.com>
2020-06-17 17:00:45 +0200

    Fetched the data from our fake database

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { createStore } from "redux";
 import { Provider } from "react-redux";
 import reducer from "./reducers";
 import middleware from "./middlewares"
+
 const store = createStore(reducer, middleware);
 
 ReactDOM.render(
