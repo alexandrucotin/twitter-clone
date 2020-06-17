79ca040f70a75638eab66125a2fdb26ea0ab6c21
Alexandru Cotin <a.cotin96@gmail.com>
2020-06-17 18:27:35 +0200

    Added loading bar

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import authedUsers from "./authedUser";
 import users from "./users";
 import tweets from "./tweets";
+import { loadingBarReducer } from "react-redux-loading";
 
 export default combineReducers({
   authedUsers,
   users,
   tweets,
+  loadingBar : loadingBarReducer, 
 });
